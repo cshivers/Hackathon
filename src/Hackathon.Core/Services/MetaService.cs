7f63f2d7f7da8893efefa46e51f6d340e9a33bff
Matt Barber <mattbarber1994@aol.com>
2020-07-17 11:42:46 +0100

    Fixed getting ranks

--- src/Hackathon.Core/Services/MetaService.cs
+++ src/Hackathon.Core/Services/MetaService.cs
@@ -15,7 +15,7 @@ namespace Hackathon.Core.Services
         }
 
         public string GetAgentName(string id) => _metaConfig.Agents.TryGetValue(id, out string agent) ? agent : null;
-        public string GetRankTier(int id) => _metaConfig.Agents.TryGetValue(id.ToString(), out string rank) ? rank : null;
+        public string GetRankTier(int id) => _metaConfig.RankTiers.TryGetValue(id.ToString(), out string rank) ? rank : null;
         public string GetWeaponName(string id) => _metaConfig.Weapons.TryGetValue(id, out string weapon) ? weapon : null;
     }
 }
