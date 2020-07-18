fa28b83a9f99887329c2c4d3db2932c9fd28aef6
kalek <ahmedelakehal@gmail.com>
2020-07-18 16:12:40 +0100

    Added Discord Bot

---
+++ src/ValorantStatTracker/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ValorantStatTracker
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var bot = new Bot();
+            bot.RunAsync().GetAwaiter().GetResult();
+        } 
+        
+        public static string CapitalizeFirstLetter(string string1)
+        {
+            Char[] characters = string1.ToCharArray();
+            characters[0] = Char.ToUpper(characters[0]);
+            return new string(characters);
+        }
+    }
+}
